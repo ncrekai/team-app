bdb591716b25f39b1eb3ca3cbfb69558fcaf08db
Toqa <tmouhame@my.centennialcollege.ca>
2024-11-02 11:00:09 -0400

    changed variables names in server.js

--- server/server.js
+++ server/server.js
@@ -4,11 +4,8 @@ const config = require('./config/config');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.mongoUri, {
-    useNewUrlParser: true,  
-    useUnifiedTopology: true
-})
+mongoose.connect(config.MONGODB_URI)
 .then(() => console.log('Database connected successfully'))
 .catch(err => console.error(`Database connection failed: ${err.message}`))
 
-app.listen(config.port, () => console.info(`Server started on the port: ${config.port}`));
+app.listen(config.PORT, () => console.info(`Server started on the port: ${config.PORT}`));
