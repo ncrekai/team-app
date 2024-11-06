a1230dc1180565e47f81a30dfea6dbc404f26b59
Toqa <tmouhame@my.centennialcollege.ca>
2024-11-06 13:58:32 -0500

    added the trip route to the route handlers

--- server/express.js
+++ server/express.js
@@ -12,6 +12,7 @@ const bodyParser = require('body-parser');
 const assetsRouter = require("./assets-router");
 const userRoutes = require ('./routes/User')
 const contactRoutes = require ('./routes/Contact')
+const tripRoutes = require ('./routes/Trip')
 
 // Initialize the app
 const app = express()
@@ -20,13 +21,20 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
+// Additional middlewares
+app.use(cookieParser())
+app.use(compress())
+app.use(helmet())
+app.use(cors())
+
 // Server static file
 app.use('/',express.static(path.join(__dirname,'public')));
 app.use('/src',assetsRouter);
 
 // Use route handlers
-app.use('/',userRoutes);
-app.use('/',contactRoutes);
+app.use('/users',userRoutes);
+app.use('/contacts',contactRoutes);
+app.use('/trips',tripRoutes);
 
 // API endpoint
 app.get('/api/v1', (req, res) => {
@@ -39,10 +47,6 @@ app.get('/api/v1', (req, res) => {
 app.get('/*',function (req,res){
   res.sendFile(path.join(__dirname,'../public','index.html'))
 });
-// Additional middlewares
-app.use(cookieParser())
-app.use(compress())
-app.use(helmet())
-app.use(cors())
+
 
 module.exports = app;
