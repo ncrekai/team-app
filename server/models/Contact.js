168b0c158768bb2b57e01855141042b3587378e8
Toqa <tmouhame@my.centennialcollege.ca>
2024-11-06 12:15:09 -0500

    added user to contacts model

--- server/models/Contact.js
+++ server/models/Contact.js
@@ -11,13 +11,30 @@ const ContactSchema = new mongoose.Schema({
         trim: true,
         required: 'Last name is required'
     },
+    phone: {
+        type: String,
+        required: true,
+    },
     email: {
         type: String,
         trim: true,
         unique: 'Email already exists',
         match: [/.+\@.+\..+/, 'Please fill a vadil email address'],
         required: 'Email is required'
-    }
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    created: {
+        type: Date,
+        default: Date.now,
+    },
+    updated: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 module.exports = mongoose.model('Contact', ContactSchema);
