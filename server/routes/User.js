ad9ede8db6b34bcf6b74ca36b4fa1eba19823979
Toqa <tmouhame@my.centennialcollege.ca>
2024-11-07 17:24:48 -0500

    removed authmoddileware

--- server/routes/User.js
+++ server/routes/User.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const userCntrl = require('../controller/User');
-const authMiddleware = require('../middlewares/authMiddleware');
+const userController = require('../controller/User');
 
 // get the list of all users (GET)
-router.route('/').get(userCntrl.getUsers);
+router.route('/').get(userController.getUsers);
 
 // get a specific user by ID (GET)
-router.route('/:id').get(userCntrl.getUserById);
+router.route('/:id').get(userController.getUserById);
 
 // create a new user (POST)
-router.route('/').post(userCntrl.createUser);
+router.route('/').post(userController.createUser);
 
 // update a specific user by ID (PUT)
-router.route('/:id').put(userCntrl.updateUser);
+router.route('/:id').put(userController.updateUser);
 
 // delete a specific user by ID (DELETE)
-router.route('/:id').delete(userCntrl.deleteUser);
+router.route('/:id').delete(userController.deleteUser);
 
 // delete all users (DELETE)
-router.route('/').delete(userCntrl.deleteAllUsers);
+router.route('/').delete(userController.deleteAllUsers);
 
 module.exports = router;
