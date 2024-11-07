d0b8c63fd9ef2fff90d6c1ef8f0fd1dee31fd94c
Toqa <tmouhame@my.centennialcollege.ca>
2024-11-06 21:50:52 -0500

    removed some comments

--- server/config/config.js
+++ server/config/config.js
@@ -12,9 +12,6 @@ const config = {
     jwtSecret: process.env.JWT_SECRET,
 
     // MongoDB connection URI:
-    // First, attempt to use MONGO_URI from .env (e.g., a full MongoDB connection string)
-    // If MONGO_URI is not provided, check for MONGO_HOST and MONGO_PORT to build the URI
-    // Defaults to localhost with port 27017 if no other options are found
     MONGODB_URI: process.env.MONGO_URI ||
         (process.env.MONGO_HOST
             ? 'mongodb://' + process.env.MONGO_HOST + ':' + (process.env.MONGO_PORT || '27017') + '/app'
