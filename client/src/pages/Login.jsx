07b90e239103d8e5e9660cfbdbbc11d3dd82fecc
ncrekai <nrekai@my.centennialcollege.ca>
2024-11-20 16:42:48 -0500

    correcting login (overwritten during merge)

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,24 +1,25 @@
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import {useContext, useState} from 'react';
+import { AuthContext } from "../services/authContext.jsx";
 
 const Login = () => {
-   const [userLogin, setUserLogin] = useState({ username: '', password: '' })
-   const navigate = useNavigate()
-
-   const handleInput = (e) => {
-      e.preventDefault()
-      const {name, value} = e.target;
-      console.log(name, value)
-      setUserLogin({ ...userLogin, [name]: value })
-   }
+   const { handleLogin } = useContext(AuthContext);
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
+   const navigate = useNavigate();
 
    const handleSubmit = async (e) => {
-      e.preventDefault()
-      console.log('...pretending to send to api...')
-      const jsonBody = JSON.stringify(userLogin)
-      console.log(jsonBody)
-      navigate('/')
-   }
+      e.preventDefault();
+      try {
+         await handleLogin(email, password);
+         alert("Logged in successfully");
+         // Redirect to the dashboard after successful login
+         navigate('/dashboard');
+      } catch (error) {
+         console.log(error);
+         alert("Failed to login. Please check your credentials.");
+      }
+   };
 
    return (
       <div className='page-inner'>
@@ -27,11 +28,11 @@ const Login = () => {
                <form className='form-container' onSubmit={handleSubmit}>
                   <div className='input-container'>
                      <div className='input-label'>Username:</div>
-                     <input className='input-text' id='username' name='username' type='text' onChange={handleInput} />
+                     <input className='input-text' name='email' type='email' onChange={(e) => setEmail(e.target.value)} />
                   </div>
                   <div className='input-container'>
                      <div className='input-label'>Password</div>
-                     <input className='input-text' id='password' name='password' type='text' onChange={handleInput} />
+                     <input className='input-text' name='password' type='password' onChange={(e) => setPassword(e.target.value)} />
                   </div>
                   <div className='input-container'>
                      <input className='button' type='submit' value='Submit' />
