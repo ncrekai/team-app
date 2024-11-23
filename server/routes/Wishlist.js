de0210138216b6d0f6f028bf3bbe357c01c92ecd
Toqa <tmouhame@my.centennialcollege.ca>
2024-11-23 14:31:27 -0500

    create new wishlist for the user

--- server/routes/Wishlist.js
+++ server/routes/Wishlist.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const wishlistController = require('../controller/Wishlist');
 
+// Create a new wishlist for specific user (POST)
+router.post('/:userId', wishlistController.createWishlist);
+
 // Fetch general wishlists for a user (GET)
 router.get('/general/:userId', wishlistController.getGeneralWishlists);
+
+router.delete('/:userId', wishlistController.deleteWishlists);
 
 module.exports = router;
