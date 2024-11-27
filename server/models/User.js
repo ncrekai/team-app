b1a446b0f877bdb49d9f8fb91ad8e2b6c9073302
Toqa <tmouhame@my.centennialcollege.ca>
2024-11-27 15:53:56 -0500

    removed required

--- server/models/User.js
+++ server/models/User.js
@@ -6,7 +6,6 @@ const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         trim: true,
-        required: 'Name is required',
     },
     email: {
         type: String,
