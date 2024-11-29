8c3a939db46905d55f3a5010bb31d2b4f2977216
Toqa <tmouhame@my.centennialcollege.ca>
2024-11-29 10:25:56 -0500

    installed toast package

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
-import { Outlet, useLoaderData } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar.jsx';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div id='App' className='app-container'>
       <Navbar/>
       <div id='Page' className='page-container'>
-        <Outlet/>
+          <ToastContainer autoClose={1000}/>
+          <Outlet/>
       </div>
     </div>
   );
